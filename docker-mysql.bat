a41ffec8b4c5b3abcc13b22e55748dbf24f815e6
Geo | Gheorghe Fierascu <fierascu@gmail.com>
2021-06-03 07:20:32 +0300

    Update docker-mysql.bat

--- docker-mysql.bat
+++ docker-mysql.bat
@@ -1,2 +1,3 @@
-docker run --rm --name mysql-db -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
-docker run --rm --name mysql-db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=mypassword -d mysql
+docker run --rm --name dbName -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
+docker run --rm --name dbName -p 3306:3306 -e MYSQL_ROOT_PASSWORD=rootPass -d mysql
+docker run --rm --name dbName -p 3306:3306 -e MYSQL_USER=notRootUser -e MYSQL_PASSWORD=pass -e MYSQL_ROOT_PASSWORD=rootPass -d mysql
